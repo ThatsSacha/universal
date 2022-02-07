81391ba8a8a371c43f90f3809f34b9ab7ebc0db5
Sacha COHEN <contact@sacha-cohen.fr>
2022-02-07 08:01:27 +0100

    #

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StorageComponent } from './storage/storage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StorageComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule
   ],
   providers: [],
